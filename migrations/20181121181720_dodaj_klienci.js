51d8220b06676f3e8397e8ecc6006dacd2eb6021
Tomasz Podkowski <t.podkowski@merixstudio.com>
2018-11-24 14:55:41 +0100

    Fix typo.

--- migrations/20181121181720_dodaj_klienci.js
+++ migrations/20181121181720_dodaj_klienci.js
@@ -1,5 +1,4 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex
     .schema
     .createTable('Klienci', clientTable => {
@@ -11,54 +10,51 @@ exports.up = function(knex, Promise) {
       clientTable.string('nazwisko', 25).notNullable();
       clientTable.string('nip', 10).notNullable();
       clientTable.string('ulica', 36).notNullable();
-	  clientTable.string('kodPocztowy',6).notNullable();
-	  clientTable.string('Miejscowosc',12).notNullable();
+      clientTable.string('kodPocztowy', 6).notNullable();
+      clientTable.string('miejscowosc', 12).notNullable();
+    })
 
-      clientTable.timestamp('created_at').notNullable();
-    }
-		
     .createTable('Kategorie', categoryTable => {
       categoryTable.increments();
-		categoryTable.string('nazwaKategorii', 25).notNullable();
+      categoryTable.string('nazwaKategorii', 25).notNullable();
     })
-		
-	.createTable('Magazyn', magazynTable => {
+
+    .createTable('Magazyn', magazynTable => {
       magazynTable.increments();
-		magazynTable.int('idProduktu', 10).notNullable();
-		magazynTable.int('ilosc',10).notNullable();
-	})
-		
-	.createTable('Produkty', produktyTable => {
-		produktyTable.increments();
-			produktyTable.string('nazwa',25).notNullable();
-			produktyTable.int('cena', 10).notNullable();
-			produktyTable.int('idKategoria',10).notNullable();
-	})
-	
-	.createTable('SzczegolyZamowienia', detailsTable => {
-		detailsTable.increments();
-			detailsTable.int('idProduktu',10).notNullable();
-			detailsTable.int('idZamowienia', 10).notNullable();
-			detailsTable.int('ilosc',10).notNullable();
-			detailsTable.int('Rabat',10).notNullable();
-	})
-	
-	.createTable('Zamowienia', zamowieniaTable => {
-		zamowieniaTable.increments();
-			zamowieniaTable.int('idKlienta',10).notNullable();
-			zamowieniaTable.date('data', 10).notNullable();
-	})
 
+      magazynTable.integer('idProduktu', 10).notNullable();
+      magazynTable.integer('ilosc', 10).notNullable();
+    })
 
+    .createTable('Produkty', produktyTable => {
+      produktyTable.increments();
+      produktyTable.string('nazwa', 25).notNullable();
+      produktyTable.integer('cena', 10).notNullable();
+      produktyTable.integer('idKategoria', 10).notNullable();
+    })
+
+    .createTable('SzczegolyZamowienia', detailsTable => {
+      detailsTable.increments();
+      detailsTable.integer('idProduktu', 10).notNullable();
+      detailsTable.integer('idZamowienia', 10).notNullable();
+      detailsTable.integer('ilosc', 10).notNullable();
+      detailsTable.integer('Rabat', 10).notNullable();
+    })
+
+    .createTable('Zamowienia', zamowieniaTable => {
+      zamowieniaTable.increments();
+      zamowieniaTable.integer('idKlienta', 10).notNullable();
+      zamowieniaTable.date('data', 10).notNullable();
+    });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex
     .schema
     .dropTableIfExists('Klienci')
-	.dropTableIfExists('Kategorie')
-	.dropTableIfExists('Magazyn')
-	.dropTableIfExists('Produkty')
-	.dropTableIfExists('SzczegolyZamowienia')
-	.dropTableIfExists('Zamowienia')
+    .dropTableIfExists('Kategorie')
+    .dropTableIfExists('Magazyn')
+    .dropTableIfExists('Produkty')
+    .dropTableIfExists('SzczegolyZamowienia')
+    .dropTableIfExists('Zamowienia');
 };
