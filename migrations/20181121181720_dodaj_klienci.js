c5b81cef2c464bd15c7dfcf54dc642f9a135232a
tomaswozniak <tomaszwozniak14@gmail.com>
2018-11-24 12:06:06 +0100

    Update 20181121181720_dodaj_klienci.js

--- migrations/20181121181720_dodaj_klienci.js
+++ migrations/20181121181720_dodaj_klienci.js
@@ -2,28 +2,61 @@
 exports.up = function(knex, Promise) {
   return knex
     .schema
-    .createTable('klienci', clientTable => {
+    .createTable('Klienci', clientTable => {
       // Primary Key
       clientTable.increments();
 
       // Data
-      clientTable.string('imie', 250).notNullable();
-      clientTable.string('nazwisko', 250).notNullable();
-      clientTable.string('nip', 250).notNullable();
+      clientTable.string('imie', 25).notNullable();
+      clientTable.string('nazwisko', 25).notNullable();
+      clientTable.string('nip', 10).notNullable();
       clientTable.string('adres', 36).notNullable().unique();
-      clientTable.integer('rabat').notNullable().defaultTo(0);
 
       clientTable.timestamp('created_at').notNullable();
-    })
-    .createTable('kategorie', categoryTable => {
+    }
+		
+    .createTable('Kategorie', categoryTable => {
       categoryTable.increments();
-
-      categoryTable.string('nazwaKategorii', 250).notNullable();
+		categoryTable.string('nazwaKategorii', 25).notNullable();
     })
+		
+	.createTable('Magazyn', magazynTable => {
+      magazynTable.increments();
+		magazynTable.int('idProduktu', 10).notNullable();
+		magazynTable.int('ilosc',10).notNullable();
+	})
+		
+	.createTable('Produkty', produktyTable => {
+		produktyTable.increments();
+			produktyTable.string('nazwa',25).notNullable();
+			produktyTable.int('cena', 10).notNullable();
+			produktyTable.int('idKategoria',10).notNullable();
+	})
+	
+	.createTable('SzczegolyZamowienia', detailsTable => {
+		detailsTable.increments();
+			detailsTable.int('idProduktu',10).notNullable();
+			detailsTable.int('idZamowienia', 10).notNullable();
+			detailsTable.int('ilosc',10).notNullable();
+			detailsTable.int('Rabat',10).notNullable();
+	})
+	
+	.createTable('Zamowienia', zamowieniaTable => {
+		zamowieniaTable.increments();
+			zamowieniaTable.int('idKlienta',10).notNullable();
+			zamowieniaTable.date('data', 10).notNullable();
+	})
+
+
 };
 
 exports.down = function(knex, Promise) {
   return knex
     .schema
-    .dropTableIfExists('klienci');
+    .dropTableIfExists('Klienci')
+	.dropTableIfExists('Kategorie')
+	.dropTableIfExists('Magazyn')
+	.dropTableIfExists('Produkty')
+	.dropTableIfExists('SzczegolyZamowienia')
+	.dropTableIfExists('Zamowienia')
 };
